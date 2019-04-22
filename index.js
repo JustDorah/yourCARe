af8156caa9c74670baf14186dff0fe26ac84246d
Dorah <dorah.bmw@gmail.com>
2019-04-22 13:23:27 -0500

    displayed general car info

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function getCarInfo(vin, mileage) {
         })
         .then(responseJson => {
             //console.log(responseJson)
-            displayGenCar(responseJson);
+            displayGenCar(responseJson,mileage);
         })
         .catch(err => {
             console.log(err.message);
@@ -52,10 +52,41 @@ function getCarInfo(vin, mileage) {
     // displayGenCar(genCarInfo);
 }
 
-function displayGenCar(responseJson){
+function displayGenCar(responseJson, mileage){
     console.log('displayGenCar is working!!');
 
+    const results = responseJson.Results;
+    //console.log(results);
+
+//filter out the desired data
+    const manuVariable = function(results){return results.Variable === 'Manufacturer Name';};
+    const manufacturer = results.filter(manuVariable)[0];
+    console.log(manufacturer.Value, 'manufacturer');
     
+    const makVariable = function(results){return results.Variable === 'Make';};
+    const make = results.filter(makVariable)[0];
+    console.log(make.Value, 'make');
+
+    const modVariable = function(results){return results.Variable === 'Model';};
+    const model = results.filter(modVariable)[0];
+    console.log(model.Value, 'model');
+
+    const yrVariable = function(results){return results.Variable === 'Model Year';};
+    const year = results.filter(yrVariable)[0];
+    console.log(year.Value, 'year');
+
+    console.log(mileage, 'mileage');
+   
+
+    $('div .generalInfo').addClass('hidden');
+    $('div .displayGenInfo').append( `
+    <p><strong>Manufacturer</strong>: ${manufacturer.Value}</p>
+    <p><strong>Make</strong>: ${make.Value}</p>
+    <p><strong>Model</strong>: ${model.Value}</p>
+    <p><strong>Year</strong>: ${year.Value}</p>
+    <p><strong>Mileage</strong>: ${mileage}</p>`
+    );
+
 }
 
 $(appStart());
