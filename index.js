cbc3f30da29e88777eb76cc541107198def33688
Dorah <dorah.bmw@gmail.com>
2019-04-24 10:05:47 -0500

    sharing

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function getCarInfo(vin, mileage) {
         })
         .then(responseJson => {
             //console.log(responseJson)
-            displayGenCar(responseJson,mileage);
+            displayGenCar(responseJson, mileage);
         })
         .catch(err => {
             console.log(err.message);
@@ -52,40 +52,47 @@ function getCarInfo(vin, mileage) {
     // displayGenCar(genCarInfo);
 }
 
-function displayGenCar(responseJson, mileage){
+function displayGenCar(responseJson, mileage) {
     console.log('displayGenCar is working!!');
 
     const results = responseJson.Results;
     //console.log(results);
 
-//filter out the desired data
-    const manuVariable = function(results){return results.Variable === 'Manufacturer Name';};
+    //filter out the desired data
+    const manuVariable = function (results) {
+        return results.Variable === 'Manufacturer Name';
+    };
     const manufacturer = results.filter(manuVariable)[0];
     console.log(manufacturer.Value, 'manufacturer');
-    
-    const makVariable = function(results){return results.Variable === 'Make';};
+
+    const makVariable = function (results) {
+        return results.Variable === 'Make';
+    };
     const make = results.filter(makVariable)[0];
     console.log(make.Value, 'make');
 
-    const modVariable = function(results){return results.Variable === 'Model';};
+    const modVariable = function (results) {
+        return results.Variable === 'Model';
+    };
     const model = results.filter(modVariable)[0];
     console.log(model.Value, 'model');
 
-    const yrVariable = function(results){return results.Variable === 'Model Year';};
+    const yrVariable = function (results) {
+        return results.Variable === 'Model Year';
+    };
     const year = results.filter(yrVariable)[0];
     console.log(year.Value, 'year');
 
     console.log(mileage, 'mileage');
-   
+
 
     $('div .generalInfo').addClass('hidden');
-    $('div .displayGenInfo').append( `
+    $('div .displayGenInfo').append(`
     <p><strong>Manufacturer</strong>: ${manufacturer.Value}</p>
     <p><strong>Make</strong>: ${make.Value}</p>
     <p><strong>Model</strong>: ${model.Value}</p>
     <p><strong>Year</strong>: ${year.Value}</p>
-    <p><strong>Mileage</strong>: ${mileage}</p>`
-    );
+    <p><strong>Mileage</strong>: ${mileage}</p>`);
 }
 
 //data display from response.js
@@ -96,27 +103,74 @@ const data = returned.data;
 
 let holder = {};
 let output = [];
-let newArr =[];
+let newArr = [];
+let nData = [];
 
-function removeDublicates(data){
-    let nData = [];
+function removeDublicates(d) {
+
     let i;
-    for(i = 0; i< data.length; i++){
-        if(nData.indexOf(data[i].desc) == -1){
+    for (i = 0; i < data.length; i++) {
+        if (nData.indexOf(data[i].desc) == -1) {
             nData.push(data[i].desc);
-        }       
+        }
     }
-    group(nData);
-    return nData;
-    
-}
-console.log(removeDublicates(data));
 
-function group(nData){
-    let i;
-    for(i = 0; i < nData.length; i++){
-        console.log(nData[i]);
+
+
+    return nData;
+    //loop over data
+    //push description into the array
+    //if description
+
+
+}
+removeDublicates(data);
+//console.log(nData[0]);
+//console.log(removeDublicates(nData));
+group(nData);
+
+function group(nData) {
+    console.log('group works');
+
+    //console.log(data);
+
+    //data.forEach(function(){
+    let wantedData = [];
+    let i = 0;
+    // console.log(data[1].desc)
+    //typeof(wantedData);
+    //console.log(typeof (wantedData));
+
+    let desc = data[i].desc;
+    let mileage = data[i].due_mileage;
+        
+    wantedData[i] = {
+        'name': `${desc}`,
+        'mileage': [mileage]        
+    };
+
+
+    for (i = 0; i < data.length; i++) {
+        console.log(wantedData[i].mileage);
+       
+        if(wantedData[i].name == data[i].desc){
+
+            if(wantedData[i].mileage == data[i].due_mileage){
+                wantedData[i].mileage.push(data[i].due_mileage);
+            }
+        }
+      
     }
+
+console.log(wantedData[0].mileage);
+    
+
+        
+    
+        //wantedData[0].mileage.push(10000);
+   // console.log(wantedData[4].mileage, wantedData[4].name);
+
+
 }
 
 /*
