cfab6dfc39210996b1e81d39d4603c7b326413c6
Dorah <dorah.bmw@gmail.com>
2019-04-26 08:15:23 -0500

    added fetch to getCarMaintenance. works

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 //I can get the maintenance schedule from here
 const maintenanceBaseURL = 'https://api.carmd.com/v3.0/maint';
+//Assists with getting data until a backend can be built for backend requests
+const forNonProductionHelp = 'https://cors-anywhere.herokuapp.com/';
 
 //I can get general car information from here
 const genInfoBaseUrl = 'https://vpic.nhtsa.dot.gov/api/vehicles/decodevinextended';
@@ -13,20 +15,20 @@ function appStart() {
     $('#vinForm').submit(function (event) {
         event.preventDefault();
         const vin = $('#vinForm #enterVin').val();
-        const mileage = $('#vinForm #enterMileage').val();
-        console.log(vin, mileage);
+        const milesDriven = $('#vinForm #enterMileage').val();
+        //console.log(vin, milesDriven);
 
-        getCarInfo(vin, mileage);
-        //getMaintenance(vin, mileage);
-        getCarMaintenance();
+        getCarInfo(vin, milesDriven);
+
+        getCarMaintenance(vin, milesDriven);
     });
 }
 
 //gets general car information
-function getCarInfo(vin, mileage) {
-    const params = {
-        vinNum: vin,
-    };
+function getCarInfo(vin, milesDriven) {
+    /* const params = {
+         vinNum: vin,
+     };*/
     const genCarInfoQuery = `${vin}?format=json`;
     //console.log(genCarInfoQuery);
 
@@ -42,18 +44,15 @@ function getCarInfo(vin, mileage) {
         })
         .then(responseJson => {
             //console.log(responseJson)
-            displayGenCar(responseJson, mileage);
+            displayGenCar(responseJson, milesDriven);
         })
         .catch(err => {
             console.log(err.message);
         });
-    // const genCarInfo = responseJson;
-    // console.log(genCarInfo);
-    // displayGenCar(genCarInfo);
 }
 
 //displays general car information
-function displayGenCar(responseJson, mileage) {
+function displayGenCar(responseJson, milesDriven) {
     console.log('displayGenCar is working!!');
 
     const results = responseJson.Results;
@@ -64,27 +63,27 @@ function displayGenCar(responseJson, mileage) {
         return results.Variable === 'Manufacturer Name';
     };
     const manufacturer = results.filter(manuVariable)[0];
-    console.log(manufacturer.Value, 'manufacturer');
+    //console.log(manufacturer.Value, 'manufacturer');
 
     const makVariable = function (results) {
         return results.Variable === 'Make';
     };
     const make = results.filter(makVariable)[0];
-    console.log(make.Value, 'make');
+    //console.log(make.Value, 'make');
 
     const modVariable = function (results) {
         return results.Variable === 'Model';
     };
     const model = results.filter(modVariable)[0];
-    console.log(model.Value, 'model');
+    //console.log(model.Value, 'model');
 
     const yrVariable = function (results) {
         return results.Variable === 'Model Year';
     };
     const year = results.filter(yrVariable)[0];
-    console.log(year.Value, 'year');
 
-    console.log(mileage, 'mileage');
+    //console.log(year.Value, 'year');
+    //console.log(milesDriven, 'mileage');
 
 
     $('div .generalInfo').addClass('hidden');
@@ -93,48 +92,95 @@ function displayGenCar(responseJson, mileage) {
     <p><strong>Make</strong>: ${make.Value}</p>
     <p><strong>Model</strong>: ${model.Value}</p>
     <p><strong>Year</strong>: ${year.Value}</p>
-    <p><strong>Mileage</strong>: ${mileage}</p>`);
+    <p><strong>Mileage</strong>: ${milesDriven}</p>`);
 }
 
 //data display from response.js
 //console.log(returned.data[0].desc);
 
-function getCarMaintenance(){
-const data = returned.data;//data in json
-console.log(data);
-const condensed = [];
-console.log(data[0].desc);
-console.log(data[0].hasOwnProperty('desc'));
-
-//push new objects into condensed
-//but not those with duplicate desc
-//push mileage value from duplicate desc into object in condensed
-for(let obj in data){
-    let duplicate = false; // flag: add new obj or note?
+function getCarMaintenance(vin, milesDriven) {
     
-    for (let i = 0; i < condensed.length; i++){
-        if(condensed[i].hasOwnProperty('desc')){
-            if (condensed[i].desc == data[obj].desc){
-                condensed[i].mileage.push(data[obj].due_mileage);
-                duplicate = true;
+
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'authorization': 'Basic YTExYTM0MDYtM2Q4My00ZGM0LWI4ZjktMzc2NjIwNjBmMzEx',
+        'partner-token': 'c59e765670124519b76cb7f456879fa5'
+    }
+    //console.log(headers);
+
+    const myInit = {
+        method: 'GET',
+        headers
+    };
+    /*
+    const maintenanceBaseURL = 'https://api.carmd.com/v3.0/maint';
+
+    const forNonProductionHelp = 'https://cors-anywhere.herokuapp.com/'; */
+
+    
+    const maintInfoQuery = `vin=${vin}&mileage=${milesDriven}`;
+    
+    const urlMaint = forNonProductionHelp + maintenanceBaseURL + '?' + maintInfoQuery;
+
+    fetch(urlMaint, myInit)
+    .then(response => {
+        if(response.ok){
+            
+            return response.json();
+        }
+        //console.log(response);
+        throw new Error(response);
+    })
+    .then(responseJson => {
+        console.log(responseJson);//cleanOutData(responseJson)
+        //console.log(responseJson.message.message, '!!!!!');
+    })
+    .catch(err => { console.log('error', err)}
+    );
+
+}
+
+
+/*
+
+
+
+    const data = returned.data; //data in json
+    console.log(data);
+    const condensed = [];
+    console.log(data[0].desc);
+    console.log(data[0].hasOwnProperty('desc'));
+
+    //push new objects into condensed
+    //but not those with duplicate desc
+    //push mileage value from duplicate desc into object in condensed
+    for (let obj in data) {
+        let duplicate = false; // flag: add new obj or note?
+
+        for (let i = 0; i < condensed.length; i++) {
+            if (condensed[i].hasOwnProperty('desc')) {
+                if (condensed[i].desc == data[obj].desc) {
+                    condensed[i].mileage.push(data[obj].due_mileage);
+                    duplicate = true;
+                }
             }
         }
-    }    
-    if(!duplicate){
-        let newObj = {};
-        newObj.desc = data[obj].desc;
-        newObj.mileage = [data[obj].due_mileage];
-        condensed.push(newObj);
+        if (!duplicate) {
+            let newObj = {};
+            newObj.desc = data[obj].desc;
+            newObj.mileage = [data[obj].due_mileage];
+            condensed.push(newObj);
+        }
     }
-}
-displayCarMaintenance(condensed);
-}
+    displayCarMaintenance(condensed); */
 
 
 
-function displayCarMaintenance(condensed){
+
+function displayCarMaintenance(condensed) {
     console.log(condensed);
-    for(let i = 0; i < condensed.length; i++){
+    for (let i = 0; i < condensed.length; i++) {
 
         let mileage = condensed[i].mileage;
 
@@ -142,11 +188,11 @@ function displayCarMaintenance(condensed){
             <td>${condensed[i].desc}</td>
             <td id="displayMileages${i}"></td>
             </tr>`);
-        for( let j = 0; j < mileage.length; j++){
+        for (let j = 0; j < mileage.length; j++) {
             $(`#displayMileages${i}`).append(`<td>${mileage[j]}</td>`);
-        console.log(condensed[i].mileage); 
-        }         
-    };    
+            console.log(condensed[i].mileage);
+        }
+    };
 }
 
 
